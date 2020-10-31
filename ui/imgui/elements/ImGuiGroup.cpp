553a75bf77976a5b6149d47eba5a5c4406208666
petr <xflajs00@stud.fit.vutbr.cz>
2020-10-31 21:23:17 +0100

    add element group

---
+++ ui/imgui/elements/ImGuiGroup.cpp
@@ -0,0 +1,23 @@
+//
+// Created by petr on 10/31/20.
+//
+
+#include "ImGuiGroup.h"
+#include <imgui.h>
+
+namespace pf::ui {
+
+ImGuiGroup::ImGuiGroup(const std::string &elementName, const std::string &caption)
+    : ImGuiElement(elementName), ImGuiContainer(elementName),
+      ImGuiCaptionedElement(elementName, caption) {}
+
+void ImGuiGroup::render() {
+  ImGui::BeginGroup();
+  ImGui::Text("%s:", getCaption().c_str());
+  ImGui::Separator();
+  std::ranges::for_each(getChildren(), [](auto &child) { child.get().render(); });
+  ImGui::Separator();
+  ImGui::EndGroup();
+}
+
+}// namespace pf::ui
