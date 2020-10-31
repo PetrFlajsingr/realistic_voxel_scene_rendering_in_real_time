267cca7b3c13c28e803dd46cb213d6faaa2d5176
petr <xflajs00@stud.fit.vutbr.cz>
2020-10-31 19:50:50 +0100

    add button types

--- ui/imgui/elements/ImGuiButton.cpp
+++ ui/imgui/elements/ImGuiButton.cpp
@@ -7,11 +7,33 @@
 
 namespace pf::ui {
 
-ImGuiButton::ImGuiButton(const std::string &name, std::string caption, const ImVec2 &size)
+ImGuiButton::ImGuiButton(const std::string &name, std::string caption, ButtonType buttonType,
+                         const ImVec2 &size)
     : ImGuiElement(name), ImGuiCaptionedElement(name, std::move(caption)),
-      ImGuiResizableElement(name, size) {}
+      ImGuiResizableElement(name, size), type(buttonType) {}
 
 void ImGuiButton::render() {
-  if (ImGui::Button(getCaption().c_str(), getSize())) { onClick(); }
+  switch (type) {
+    case ButtonType::Normal:
+      if (ImGui::Button(getCaption().c_str(), getSize())) { onClick(); }
+      break;
+    case ButtonType::Small:
+      if (ImGui::SmallButton(getCaption().c_str())) { onClick(); }
+      break;
+    case ButtonType::ArrowUp:
+      if (ImGui::ArrowButton(getCaption().c_str(), ImGuiDir_::ImGuiDir_Up)) { onClick(); }
+      break;
+    case ButtonType::ArrowLeft:
+      if (ImGui::ArrowButton(getCaption().c_str(), ImGuiDir_::ImGuiDir_Left)) { onClick(); }
+      break;
+    case ButtonType::ArrowRight:
+      if (ImGui::ArrowButton(getCaption().c_str(), ImGuiDir_::ImGuiDir_Right)) { onClick(); }
+      break;
+    case ButtonType::ArrowDown:
+      if (ImGui::ArrowButton(getCaption().c_str(), ImGuiDir_::ImGuiDir_Down)) { onClick(); }
+      break;
+  }
 }
+ButtonType ImGuiButton::getType() const { return type; }
+void ImGuiButton::setType(ButtonType buttonType) { type = buttonType; }
 }// namespace pf::ui
--- ui/imgui/elements/ImGuiButton.h
+++ ui/imgui/elements/ImGuiButton.h
@@ -11,15 +11,23 @@
 #include <imgui.h>
 namespace pf::ui {
 
+enum class ButtonType { Normal, Small, ArrowUp, ArrowLeft, ArrowRight, ArrowDown };
+
+// TODO: multiple listeners
 class ImGuiButton : public ImGuiCaptionedElement, public ImGuiResizableElement {
  public:
-  ImGuiButton(const std::string &name, std::string caption, const ImVec2 &size = {0, 0});
+  ImGuiButton(const std::string &name, std::string caption,
+              ButtonType buttonType = ButtonType::Normal, const ImVec2 &size = {0, 0});
   void render() override;
 
   void setOnClick(std::invocable auto fnc) { onClick = fnc; }
 
+  [[nodiscard]] ButtonType getType() const;
+  void setType(ButtonType type);
+
  private:
   std::function<void()> onClick = [] {};
+  ButtonType type;
 };
 
 }// namespace pf::ui
