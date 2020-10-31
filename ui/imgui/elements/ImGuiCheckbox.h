93b2754e6f3af4c23f70b7979e49e80ba44b1e2c
petr <xflajs00@stud.fit.vutbr.cz>
2020-10-31 21:05:12 +0100

    add checkbox

---
+++ ui/imgui/elements/ImGuiCheckbox.h
@@ -0,0 +1,18 @@
+//
+// Created by petr on 10/31/20.
+//
+
+#ifndef REALISTIC_VOXEL_RENDERING_UI_IMGUI_ELEMENTS_IMGUICHECKBOX_H
+#define REALISTIC_VOXEL_RENDERING_UI_IMGUI_ELEMENTS_IMGUICHECKBOX_H
+
+#include "interface/ImGuiCaptionedElement.h"
+#include "interface/ImGuiValueObservableElement.h"
+
+namespace pf::ui {
+class ImGuiCheckbox : public ImGuiValueObservableElement<bool>, public ImGuiCaptionedElement {
+ public:
+  ImGuiCheckbox(const std::string &elementName, const std::string &caption, bool value = false);
+  void render() override;
+};
+}// namespace pf::ui
+#endif//REALISTIC_VOXEL_RENDERING_UI_IMGUI_ELEMENTS_IMGUICHECKBOX_H
